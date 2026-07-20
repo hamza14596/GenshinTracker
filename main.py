6c01e3ad49795642918d0d2ff530b967980ab8df
hamza <hmzuuu87@gmail.com>
2026-07-20 23:14:53 +0500

    added alot of stuff, artifact substats, images, main stats, a back button and alot of other stuff, now started workin on account details

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import streamlit as st
 from lookup import loadup_data, get_character_name, get_char_icon, get_item_name, get_item_icon_url,get_stat_name
 
 artifact_name_shown = False
+artifact_substats = []
 
 uid = st.text_input('Enter ur UID: ')
 
@@ -26,8 +27,35 @@ def get_player_data(uid):
 
 response = get_player_data(uid)
 if response.status_code == 200:
-    data = response.json()       
+    data = response.json()     
     if st.session_state.get('current_char') == None:
+        if st.button('Account Details'):
+            player_info = data.get('playerInfo')
+            player_name = player_info.get('nickname','N/A')
+            player_signature = player_info.get('signature','N/A')
+
+            player_pp = player_info.get('profilePicture')
+            player_avatar = player_pp.get('avatarId')
+            player_avatar_icon = None
+            if player_pp:
+                player_avatar_icon = get_char_icon(str(player_avatar), char_data)
+
+            player_AR = player_info.get('level','N/A')
+            player_worldlvl = player_info.get('worldLevel','N/A')
+            player_achievements = player_info.get('finishAchievementNum','N/A')
+
+            player_abyss_T = player_info.get('towerFloorIndex','N/A')
+            player_abyss_L = player_info.get('towerLevelIndex','N/A')
+
+            player_theatre_level = player_info.get('fetterCount')
+            player_theatre_Index = player_info.get('stygianIndex')
+            player_theatre_time = player_info.get('stygianSeconds')
+
+            st.subheader(player_name)
+            st.subheader(f'**| AR:** {player_AR} **| World Level:** {player_worldlvl} |')
+            st.image(player_avatar_icon,width=150)
+            st.write(player_signature)
+
 
         for char in data.get("avatarInfoList", []):
             char_id = char.get("avatarId")
@@ -45,6 +73,7 @@ if response.status_code == 200:
                 st.rerun()
             char_icon_url = get_char_icon(char_id, char_data) 
             st.image(char_icon_url, width=100)
+            st.subheader('')
     else:   
             if st.button('Character List'):
                 del st.session_state['current_char']
@@ -71,9 +100,9 @@ if response.status_code == 200:
             weapon_name = get_item_name(str(weapon_dict.get('flat').get('nameTextMapHash')), loc_data)
             weapon_image_url = get_item_icon_url(weapon_dict.get('flat').get('icon'))
             st.write(char_name,'Lv :',char_level,' | Constellations: ', constellation_num)
-            st.image(char_icon_url, width=300)
-            st.subheader('Weapon: ',weapon_name)
-            st.image(weapon_image_url, width=150)
+            st.image(char_icon_url, width=200)
+            st.subheader(f'Weapon: {weapon_name}')
+            st.image(weapon_image_url, width=100)
             for artifact in artifacts:
                 flat = artifact.get('flat',{})
 
@@ -93,8 +122,13 @@ if response.status_code == 200:
                     
                 artifact_url = get_item_icon_url(flat.get('icon'))
                 artifact_main = flat.get('reliquaryMainstat',{})
+                artifact_substats = flat.get('reliquarySubstats',[])
                 st.image(artifact_url, width=100)
-                st.write(get_stat_name(artifact_main.get('mainPropId')), artifact_main.get('statValue'))
+                main_stat_name = get_stat_name(artifact_main.get('mainPropId'))
+                main_stat_val =  str(artifact_main.get('statValue'))
+                st.write(f"**{main_stat_name}** {main_stat_val}")
+                for substat in artifact_substats:
+                    st.write('-'+get_stat_name(substat.get('appendPropId')),str(substat.get('statValue')))
                 st.subheader('')
  
  
