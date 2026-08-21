73f53587757744bdd7207cdfc3dbe67b49051e3a
hamza <hmzuuu87@gmail.com>
2026-08-21 15:11:48 +0500

    .

--- api/index.py
+++ api/index.py
@@ -1,8 +1,11 @@
+import os
 from flask import Flask, render_template, request, redirect, url_for
-from lookup import *
 
 
-app = Flask(__name__)
+from api.lookup import *
+
+template_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), 'templates'))
+app = Flask(__name__, template_folder=template_dir)
 @app.route('/',methods=['GET','POST'])
 def home():
     if request.method == 'POST':
